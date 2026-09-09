c258a646339a4c211afca66aec0a3cc43b907069
osawasthere <osaspersonal@gmail.com>
2026-09-09 13:25:41 -0500

    my actions have consequences... must fix when I get the time after work...

--- xt.js
+++ xt.js
@@ -19,9 +19,54 @@ console.log(times)
 .split(':') splits into array of numbers
 switch statement uses array.length to decide what conversion for each grouping of time
 everything becomes seconds
+
+switch (timestamp.length){
+case(3):
+
+seconds += timestamp[2] * 3600)
+// fallthrough is intentional
+
+case(2):
+
+seconds += (timestamp[1] * 60)
+caes(1):
+
+seconds += timestamp[0]
+
+}
+
+
 then switch statement turns seconds to minutes starting from the
 largest ((seconds)// 31556952 >= 1){ array.push that same
 equation, and pass seconds % 31556952 down the line to months
+
+function convert(rate, seconds){
+    if (Math.floor(seconds/rate) >= 1){
+    finalTimeStampArray.push(Math.floor(seconds/rate))
+    return (seconds%rate)
+    }
+}
+
+
+    if (seconds//31556952 >= 1){
+        finalTimeStampArray.push(seconds//31556952)
+        seconds = seconds%31556952}
+    if (seconds // 604800 >= 1){
+        finalTimeStampArray.push(seconds//604800)
+        seconds = seconds % 604800}
+    if (seconds//86400 >= 1){
+        finalTimeStampArray.push(seconds//86400)
+        seconds = seconds % 86400}
+    if (seconds//3600 >= 1){
+        finalTimeStampArray.push(seconds//3600)
+        seconds = seconds % 3600}
+    if (seconds//60 >= 1){
+        finalTimeStampArray.push(seconds//60)
+        seconds = seconds % 60}
+    finalTimeStampArray.push(seconds//1)
+    
+
+
 
 seconds // 2629743.83 for month
 seconds // 604800 for week
